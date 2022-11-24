8fd29c16226e6b98d0afe4743137aeb1e1b85f35
mo-aden <mohamedaden17@outlook.com>
2022-11-24 16:09:18 -0700

    completed the app functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+//import the generateMrkDown fn
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give a brief description about the project explaining how your projects works",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How does the user install all the application dependencies e.g. inquirer?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How does the users run console log tests? (npm test)",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What is type of license does your project have? (use arrow keys for selecting) ",
+    choices: ["MIT", "APACHE_2.0", "BSD_3--Clause", "NONE"],
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "GitHub",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "fileName",
+    message: "Enter the name of the README file to be generated?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    console.log(`Generating README file ...`);
+
+    const readmeFileName = response.fileName;
+
+    // writeToFile(response.fileName.md, generateMarkdown({ ...response }));
+    writeToFile("readmeFileName.md", generateMarkdown({ ...response }));
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "NONE") {
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "NONE") {
+    return `[License](#License)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "NONE") {
+    return `## License
+    This project is licensed under ${license}`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data);
+  return `
+  # ${data.title}
+
+  ## Description 
+  ${data.description}
+
+
+  ## Table of Contents
+
+  * [Installation](#Installation)
+  * [Testing](#Testing) 
+  * [License](#License)
+  * [Contributions](#Contributions)
+  * [Questions](#Questions)
+
+  
+  ## Installation
+  Enter this command ${data.installation} to install dependencies.
+
+
+  ## Testing
+  Run this command to test/run the application = ${data.usage} 
+
+  ##Licence
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseBadge(data.license)}\n
+
+
+  ## Contributions
+  N/A
+
+
+  ## Questions
+
+  Please visit this GitHub link to see more on how this application is built [GitHub/${data.GitHub}](https://github.com/${data.GitHub})\n
+
+
+  For additional information, please reach out to me in this email address ${data.email}.
 
 `;
 }
